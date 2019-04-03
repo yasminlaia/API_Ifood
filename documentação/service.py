3989e61cc1d38350f737354270e1cbc8b60ec49a
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2019-04-03 18:55:15 -0300

    Create service.py

---
+++ documentação/service.py
@@ -0,0 +1,5 @@
+Este arquivo realiza um request de uma url com json, aonde autentica o acesso do token que é fornecido pelo site de desenvolvedor do Ifood.
+Além disso, tem funções que tem como objetivo utilizar essa autenticação do OAuth2. Elas são: 
+
+1. Função "define_local_id) para puxar o CEP do usuário 
+2. Função "lista_restaurante" para retornar a lista de restaurantes disponíveis
