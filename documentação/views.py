316d5d11d19cfaa3f1753bb4e3e263a1163d6863
yasminlaia <30813654+yasminlaia@users.noreply.github.com>
2019-04-03 19:13:16 -0300

    Create views.py

---
+++ documentação/views.py
@@ -0,0 +1,9 @@
+Este arquivo mostra as funções que fazem busca de restaurantes utilizando CEP.
+
+1. função "get": mostra uma função de quando é digitado o CEP, retorna uma mensagem de status 400 ao ser feito o request, que
+significa que não encontrou nenhum valor digitado e que deu erro na requisição
+
+2. Se a requisição for feita com sucesso, o código que irá retornar será status 200.
+
+3. Utilização dos blueprints, gravam operações para serem executadas quando registradas em um aplicativo
+
